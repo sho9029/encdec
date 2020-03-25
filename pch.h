31bb86d8b354f9798045f1375b1d22b08ea6491b
Yatabe Sho <yatabe_9029@yahoo.co.jp>
2020-03-25 23:34:04 +0900

    v1.0.1

--- pch.h
+++ pch.h
@@ -1,15 +1,9 @@
 #pragma once
 
-#include <algorithm>
-#include <cctype>
-#include <codecvt>
 #include <fstream>
 #include <iostream>
-#include <random>
-#include <string>
 #include <sstream>
 #include <vector>
-#include <Windows.h>
 #include "boost/multiprecision/cpp_int.hpp"
 
 using namespace std;
